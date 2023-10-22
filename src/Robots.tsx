975ba8e5a64ae26aff644850e9cd9a28fac23c94
pchild <childrep@vt.edu>
2023-10-21 21:47:55 -0400

    More robots info

--- src/Robots.tsx
+++ src/Robots.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import { Grid, Paper, Typography } from "@mui/material";
+import { Button, Grid, Paper, Stack, Typography, Link } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
 import robot2023 from "./img/robots/2023.jpg";
 import robot2022 from "./img/robots/2022.jpg";
@@ -51,12 +54,22 @@ const robots: RobotInfo[] = [
     cadLink:
       "https://cad.onshape.com/documents/ba977eb0e20aee4635a57eaa/w/a56a7fbc56f14dbcbff30769",
     codeLink: "https://github.com/team401/2023-Robot-Code",
+    techBinder:
+      "https://drive.google.com/file/d/1XBMxCsUlEWbWIso0fsR4-QgTbwTDzjae/view?usp=share_link",
   }),
   new RobotInfo(2022, "Jörmungandr ", {
     record: "59-21-1",
+    weight: "124.8 lb",
     img: robot2022,
+    codeLink: "https://github.com/team401/2022-Robot-Code",
+    cadLink:
+      "https://cad.onshape.com/documents/489185223ddbfec8c89a2277/w/632bf353cba19a51e989f488",
   }),
-  new RobotInfo(2021, "Glycon"),
+  new RobotInfo(2021, "Glycon", {
+    record: "6-5-0",
+    cadLink:
+      "https://cad.onshape.com/documents/84707311327861b9fdce8a73/w/652c81d4db21c84dc91a591d",
+  }),
   new RobotInfo(2020, "Leviathan"),
   new RobotInfo(2019, "Ouroboros"),
   new RobotInfo(2018, "Apophsis"),
@@ -76,42 +89,81 @@ export default function Robots() {
                   <img
                     src={robot.img}
                     loading="lazy"
-                    width="100%"
-                    style={{ maxWidth: "50vw" }}
+                    style={{ maxHeight: 500, maxWidth: "100%" }}
                   ></img>
                 </Grid>
                 <Grid xs={12} item md={robot.img ? 6 : 12}>
-                  <Paper sx={{ p: 3, minHeight: "100%" }}>
-                    <Typography
-                      variant="h4"
-                      align="left"
-                      color="text.secondary"
-                      component="h1"
-                      paddingBottom={2}
-                    >
-                      {robot.year} - {robot.name}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      align="left"
-                      color="text.secondary"
-                      fontSize="large"
-                      component="p"
-                      visibility={robot.record ? "visible" : "hidden"}
-                    >
-                      Record: {robot.record}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      align="left"
-                      color="text.secondary"
-                      fontSize="large"
-                      component="p"
-                      visibility={robot.notes ? "visible" : "hidden"}
-                    >
-                      Notes: {robot.notes}
-                    </Typography>
-                  </Paper>
+                  <Card
+                    sx={{
+                      minHeight: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <CardContent sx={{ minHeight: "100%" }}>
+                      <Typography
+                        variant="h4"
+                        align="left"
+                        color="text.secondary"
+                        component="h1"
+                        paddingBottom={2}
+                      >
+                        {robot.year} - {robot.name}
+                      </Typography>
+                      {robot.record && (
+                        <Typography
+                          variant="h5"
+                          align="left"
+                          color="text.secondary"
+                          component="h1"
+                        >
+                          Record: {robot.record}
+                        </Typography>
+                      )}
+                      {robot.notes && (
+                        <Typography
+                          variant="h5"
+                          align="left"
+                          color="text.secondary"
+                          component="h1"
+                        >
+                          Notes: {robot.notes}
+                        </Typography>
+                      )}
+                    </CardContent>
+                    <CardActions>
+                      <Stack direction="row" spacing={1}>
+                        {robot.cadLink && (
+                          <Button
+                            component={Link}
+                            href={robot.cadLink}
+                            variant="outlined"
+                          >
+                            CAD Model
+                          </Button>
+                        )}
+                        {robot.techBinder && (
+                          <Button
+                            component={Link}
+                            href={robot.techBinder}
+                            variant="outlined"
+                          >
+                            Tech Binder
+                          </Button>
+                        )}
+                        {robot.codeLink && (
+                          <Button
+                            component={Link}
+                            href={robot.codeLink}
+                            variant="outlined"
+                          >
+                            Robot Code
+                          </Button>
+                        )}
+                      </Stack>
+                    </CardActions>
+                  </Card>
                 </Grid>
               </Grid>
             ))}
