af1af0a54dca0df0dbd865ee4da60999bd3a419a
pchild <childrep@vt.edu>
2023-10-24 20:42:40 -0400

    Add a few more team resources

--- src/ResourcesData.tsx
+++ src/ResourcesData.tsx
@@ -2,6 +2,10 @@ import gantt from "./img/resources/gantt.png";
 import chsStats from "./img/resources/chsStats.png";
 import vouchers from "./img/resources/vouchers.png";
 import wiring from "./img/resources/wiring.png";
+import firstChoice from "./img/resources/firstChoice.png";
+import taxFilings from "./img/resources/taxFilings.png";
+import grantTracking from "./img/resources/grantTracking.png";
+import batteryBOM from "./img/resources/batteryBOM.png";
 
 class Resource {
   link: string;
@@ -58,11 +62,35 @@ export const ResourcesList: ResourceSection[] = [
         "A spreadsheet for tracking FRC Kit of Parts voucher usage to help teams get the most out of the virtual kit.",
         vouchers
       ),
+      new Resource(
+        "https://docs.google.com/spreadsheets/d/1MMs0xswVGVvtkZm4cmEuLiZ5mZB6jAHpzYPLVGaRHug/edit?usp=sharing",
+        "FIRST Choice Priority List Builder",
+        "A simple sheet for building FIRST Choice priority lists and checking points totals before submtting.",
+        firstChoice
+      ),
+      new Resource(
+        "https://docs.google.com/spreadsheets/d/1oaOYT3wrMLAetd9od2UxNQryoq30J9gsSHkwR5je_YM/edit?usp=sharing",
+        "FRC District Tax Filings",
+        "Tracking IRS 990 Filings for US FRC District Organizations",
+        taxFilings
+      ),
+      new Resource(
+        "https://docs.google.com/spreadsheets/d/1G1QdTnZ0QEv1-qg83_ftr5lTGCiLnGkAfJLeh3I_6vk/edit?usp=sharing",
+        "FRC Grant Tracking",
+        "A worksheet for tracking applications and submission status for many FRC grants.",
+        grantTracking
+      ),
+      new Resource(
+        "https://docs.google.com/spreadsheets/d/1a0TK5J_OTV1hOVL-Qj_CP3aYUR3jRqawvgHWTQ2aMPQ/edit?usp=sharing",
+        "Team 401 Battery BOM",
+        "Our preferred battery construction parts and where to buy them.",
+        batteryBOM
+      ),
     ]
   ),
   new ResourceSection(
     "Community Resources",
-    "These resources are developed and maintained by the FRC community and we use on Team 401.",
+    "These resources are developed and maintained by the FRC community and recommended by Team 401.",
     []
   ),
 ];
