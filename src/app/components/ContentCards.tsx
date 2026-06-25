6153257054c8219413eba066c0e656c68d191104
gback <gback@vt.edu>
2026-06-24 21:58:13 -0400

    Migrate resources page to MDX

--- src/app/components/ContentCards.tsx
+++ src/app/components/ContentCards.tsx
@@ -1,5 +1,7 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, Grid, Link, Typography } from "@mui/material";
 import type { PropsWithChildren, ReactNode } from "react";
+
+import RespImage from "./RespImage";
 
 export function ContentCard({
   children,
@@ -39,10 +41,13 @@ export function ContentCardLead(props: PropsWithChildren<unknown>) {
   );
 }
 
-export function ContentCardGrid(props: PropsWithChildren<unknown>) {
+export function ContentCardGrid({
+  children,
+  padded = false,
+}: PropsWithChildren<{ padded?: boolean }>) {
   return (
-    <Grid container item xs={12}>
-      {props.children}
+    <Grid container item xs={12} sx={{ pt: padded ? 1 : 0 }}>
+      {children}
     </Grid>
   );
 }
@@ -76,6 +81,55 @@ export function ContentCardGridItem({
         >
           {children}
         </Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+export function Resource({
+  children,
+  img,
+  link,
+  loading,
+  title,
+}: PropsWithChildren<{
+  img?: string;
+  link: string;
+  loading?: "eager" | "lazy";
+  title: ReactNode;
+}>) {
+  return (
+    <Grid item container md={6} xs={12}>
+      <Grid
+        container
+        item
+        sx={{ m: 1, px: 1, pb: { xs: 8, sm: 42, md: 10, lg: 1 } }}
+        justifyItems="start"
+        justifyContent="start"
+        flexDirection="column"
+      >
+        <Link href={link} target="_blank">
+          <Typography variant="h5" align="left" component="h1">
+            {title}
+          </Typography>
+        </Link>
+        <Typography
+          variant="h6"
+          align="left"
+          color="text.secondary"
+          component="div"
+          sx={{
+            "& p": { m: 0, font: "inherit" },
+            "& p + p": { mt: 2 },
+          }}
+        >
+          {children}
+        </Typography>
+        {img && (
+          <Link href={link} target="_blank">
+            <RespImage src={img} loading={loading} sizes="60vh" />
+          </Link>
+        )}
       </Grid>
     </Grid>
   );
